daa1e742e7bbf56656ced278dbc552a800edb406
Dinesh Kumar E <egdedk@gmail.com>
2024-05-10 03:55:54 +0530

    Third

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [search, setSearch] = useState('');
-  const [res, setRes] = useState({'yes':'','no':''});
+  const [search, setSearch] = useState("");
+  const [res, setRes] = useState({ yes: "", no: "" });
+  const [edgecase, setEdgeCase] = useState(false);
 
   const dict = [
     {
@@ -15,22 +16,29 @@ function App() {
   ];
 
   function handleChange(e) {
-    setRes({...res,['yes']:'',['no']:''});
+    setEdgeCase(false);
+    setRes({ ...res, ["yes"]: "", ["no"]: "" });
     let { value } = e.target;
     setSearch(value);
   }
 
-
-  function wordSearch(value){
-    console.log(value)
-    let found = dict.filter((item)=>{
-      return (item.word.toLocaleLowerCase() == value.toLocaleLowerCase())
-    })
-    if(found.length>0){
-      setRes({...res,['yes']:found[0].meaning,['no']:''});
-    }
-    else{
-      setRes({...res,['no']:'Word not found in the dictionary.',['yes']:''})  
+  function wordSearch(value) {
+    if (search == "") {
+      setEdgeCase(true);
+    } else {
+      setEdgeCase(false);
+      let found = dict.filter((item) => {
+        return item.word.toLocaleLowerCase() == value.toLocaleLowerCase();
+      });
+      if (found.length > 0) {
+        setRes({ ...res, ["yes"]: found[0].meaning, ["no"]: "" });
+      } else {
+        setRes({
+          ...res,
+          ["no"]: "Word not found in the dictionary.",
+          ["yes"]: "",
+        });
+      }
     }
   }
 
@@ -43,12 +51,18 @@ function App() {
         placeholder="Search for a word..."
         onChange={handleChange}
       />
-      <button onClick={(e)=>{
-        console.log(search)
-        return wordSearch(search)}}>Search</button>
+      <button
+        onClick={(e) => {
+          console.log(search);
+          return wordSearch(search);
+        }}
+      >
+        Search
+      </button>
       <h3>Definition:</h3>
-      <div>{search&&res.yes}</div>
-      <div>{search&&res.no}</div>      
+      <div>{search && res.yes}</div>
+      <div>{search && res.no}</div>
+      <div>{edgecase && 'Word not found in the dictionary.'}</div>
     </>
   );
 }
